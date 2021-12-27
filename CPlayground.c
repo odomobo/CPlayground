4f66ba1ab7aa0b384e0189ad60e441da528fbbf4
odomobo <odomobo@gmail.com>
2021-12-27 04:21:35 -0600

    Initial commit

---
+++ CPlayground.c
@@ -0,0 +1,19 @@
+
+int simple_add_10(int value)
+{
+    int ret = value + 10;
+    return ret;
+}
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+    int c = 2;
+
+    a = simple_add_10(a);
+    b = simple_add_10(b);
+    c = simple_add_10(c);
+
+    return 0;
+}
